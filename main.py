c4eb17ba2547c0d22c998e884de41b57263666b4
Adem <guedriadem@gmail.com>
2025-04-27 20:10:57 +0300

    Finished get_json

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import re
 from datetime import date
 from tabulate import tabulate
+import json
 
 def get_data():
     headers = {'user-agent':'your actual User-Agent here'}
@@ -34,5 +35,19 @@ def get_text(type,name):
             f.write(f'Type: {type}\n')
             f.write(f'Date: {today}\n')
             f.write('='*48+'\n')
-            table = tabulate(data,headers=['Books','Price'],tablefmt="fancy_grid")
+            table = tabulate(data,headers=['Title','Price'],tablefmt="fancy_grid")
             f.write(table)
+            print('File downloaded successfully!')
+    else:
+        return False
+
+
+def get_json(name):
+    data =get_data()
+    books = [{'Title':title , 'Price':price} for title,price in data ]
+
+    if data:
+        with open(f'{name}.json','w',encoding='utf-8') as f:
+            json.dump(books,f,ensure_ascii=False)
+
+get_json('gg')
