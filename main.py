3cf406ee762f398b01b8f4d9452d85719938cf1d
Adem <guedriadem@gmail.com>
2025-04-27 21:54:24 +0300

    Finished get_url

--- main.py
+++ main.py
@@ -5,10 +5,31 @@ from datetime import date
 from tabulate import tabulate
 import json
 
-def get_data():
-    headers = {'user-agent':'your actual User-Agent here'}
+headers = {'user-agent': 'your actual User-Agent here'}
+
+def get_url(category_name):
     url = 'https://books.toscrape.com/catalogue/category/books/travel_2/index.html'
     response = requests.get(url,headers=headers)
+    if response.ok:
+        soup = BeautifulSoup(response.content,'html.parser')
+        categories_tags = soup.find('div',attrs={'class':'side_categories'})
+
+        re_categories = r'\.\.\/([\w\-]+)_\d+\/'
+        categories = re.findall(re_categories,str(categories_tags))
+
+        re_num_categorie = r'\.\.\/[\w\-]+_(\d+)\/'
+        num_categorie = re.findall(re_num_categorie,str(categories_tags))
+
+        page = list(zip(categories,num_categorie))
+        for name,number in page:
+            if name.lower() == category_name.lower():
+                url = f'https://books.toscrape.com/catalogue/category/books/{name}_{number}/index.html'
+                return url
+    else:
+        return False
+
+def get_data(category_name):
+    response = requests.get(get_url(category_name),headers=headers)
     if response.ok:
         soup = BeautifulSoup(response.content,'html.parser')
         products = soup.find_all('article',attrs={'class':'product_pod'})
@@ -26,13 +47,13 @@ def get_data():
     return False
 
 
-def get_text(type,name):
-    data = get_data()
+def get_text(category_name):
+    data = get_data(category_name)
     today = date.today().strftime('%d/%m/%Y')
 
     if data:
-        with open(f'{name}.txt','w',encoding='utf-8') as f:
-            f.write(f'Type: {type}\n')
+        with open(f'{category_name}.txt','w',encoding='utf-8') as f:
+            f.write(f'Type: {category_name}\n')
             f.write(f'Date: {today}\n')
             f.write('='*48+'\n')
             table = tabulate(data,headers=['Title','Price'],tablefmt="fancy_grid")
@@ -42,12 +63,12 @@ def get_text(type,name):
         return False
 
 
-def get_json(name):
-    data =get_data()
+def get_json(category_name):
+    data =get_data(category_name)
     books = [{'Title':title , 'Price':price} for title,price in data ]
 
     if data:
-        with open(f'{name}.json','w',encoding='utf-8') as f:
+        with open(f'{category_name}.json','w',encoding='utf-8') as f:
             json.dump(books,f,ensure_ascii=False)
-
-get_json('gg')
+    else:
+        return False
